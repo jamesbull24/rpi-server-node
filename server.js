46217ff78e4315cc8c24abc1369a44e86c49b998
jamesbull24 <jamesbull@live.com>
2021-01-29 20:36:00 +0000

    adding endpoint & html

--- server.js
+++ server.js
@@ -1,1 +1,31 @@
-const { readFile, writeFileSync } = require('fs'); // from nodeJS
+const { readFileSync, writeFileSync } = require('fs'); // from nodeJS
+
+const express = require('express');
+const app = express(); 
+
+app.get('/', (req, res) => {
+    const count = readFileSync('./count.txt', 'utf-8');
+    console.log('count', count);
+
+    const newCount = parseInt(count) + 1; //convert to Integer and increment
+    writeFileSync('./count.txt', newCount); //write back to same file
+
+    /* sending response to Endpoint */
+    res.send(`
+    <!doctype html>
+    <html lang="en">
+    <head>
+      <meta charset="utf-8">
+      <title>RPi Hosted Website Test</title>
+      <meta name="description" content="">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+    <body>
+        <h1>RasberryPi server { isOnline: true }</h1>
+        <p>This page has been viewed ${newCount} times!</p>
+    </body>
+    </html>
+    `);
+});
+
+app.listen(5000, () => console.log('http://localhost:5000/') ); 
